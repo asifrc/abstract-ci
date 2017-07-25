69951fa661c9298e4d6c4422e9ea7b8aa6ff901b
Asif R Choudhury <choudhury.asif@gmail.com>
2017-07-24 19:40:52 -0500

    Possibly make YamlFile spec more readable? Not really sure..

--- spec/abstract/state/yaml_file_spec.rb
+++ spec/abstract/state/yaml_file_spec.rb
@@ -4,23 +4,29 @@ require './lib/abstract/state/yaml_file'
 module Abstract
   module State
     describe YamlFile do
-      describe '#setup' do
+      before(:all) do
+        @expected_dir = './abstract'
+        @expected_path = './.abstract/state.yml'
+        @backend_data = {
+          'type' => 'gocd',
+          'id' => 'a01b'
+        }
+      end
+      before(:each) do
+        allow(File).to receive(:open)
+        allow(File).to receive(:write)
       end
       describe '#load' do
         it 'should return hash from state.yml file' do
-          expected_path = './.abstract/state.yml'
           file_content = "---\nbackend:\n  type: gocd\n  id: a01b\n"
           output = StringIO.new file_content
-          allow(File).to receive(:open).with(expected_path).and_yield(output)
+          allow(File).to receive(:open).with(@expected_path).and_yield(output)
 
           state = YamlFile.new
           state_hash = state.load
 
           expect(state_hash).to include(
-            'backend' =>  hash_including(
-              'type' => 'gocd',
-              'id' => 'a01b'
-            )
+            'backend' => @backend_data
           )
         end
         it 'should return an empty hash if file is not found' do
@@ -34,10 +40,8 @@ module Abstract
       end
       describe '#update' do
         it 'should create .abstract directory if it does not exist' do
-          expected_path = './.abstract/state.yml'
           output = StringIO.new ''
-          allow(File).to receive(:open).with(expected_path).and_yield(output)
-          allow(File).to receive(:write)
+          allow(File).to receive(:open).with(@expected_path).and_yield(output)
           expect(FileUtils).to receive(:mkdir_p).with('./.abstract')
 
           state = YamlFile.new
@@ -45,35 +49,25 @@ module Abstract
         end
 
         it 'should save hash as yaml to ./.abstract/state.yml' do
-          backend_data = {
-            'type' => 'gocd',
-            'id' => 'a01b'
-          }
-          expected_path = './.abstract/state.yml'
           expected_content = "---\nbackend:\n  type: gocd\n  id: a01b\n"
           output = StringIO.new ''
-          allow(File).to receive(:open).with(expected_path).and_yield(output)
-          expect(File).to receive(:write).with(expected_path, expected_content)
+          allow(File).to receive(:open).with(@expected_path).and_yield(output)
+          expect(File).to receive(:write).with(@expected_path, expected_content)
 
           state = YamlFile.new
-          state.update 'backend', backend_data
+          state.update 'backend', @backend_data
         end
 
         it 'should merge with existing state' do
-          expected_path = './.abstract/state.yml'
           original_content = "---\nexistingkey:\n  type: random\n"
           output = StringIO.new original_content
-          allow(File).to receive(:open).with(expected_path).and_yield(output)
-          backend_data = {
-            'type' => 'gocd',
-            'id' => 'a01b'
-          }
+          allow(File).to receive(:open).with(@expected_path).and_yield(output)
           backend_content = "backend:\n  type: gocd\n  id: a01b\n"
           expected_content = "#{original_content}#{backend_content}"
-          expect(File).to receive(:write).with(expected_path, expected_content)
+          expect(File).to receive(:write).with(@expected_path, expected_content)
 
           state = YamlFile.new
-          state.update 'backend', backend_data
+          state.update 'backend', @backend_data
         end
       end
     end
